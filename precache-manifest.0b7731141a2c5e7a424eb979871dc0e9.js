bc69b4c1f2c77732fab8f0d82b4860c756a7e9bb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-03-19 10:02:09 +0000

    Deploy Zhouqchao/react-dianping to github.com/Zhouqchao/react-dianping.git:gh-pages

---
+++ precache-manifest.0b7731141a2c5e7a424eb979871dc0e9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c402d524d06ba340c76c714c1f800b0",
+    "url": "/react-dianping/index.html"
+  },
+  {
+    "revision": "6ad4bd9ab94bafa31f70",
+    "url": "/react-dianping/static/css/10.23ca0c32.chunk.css"
+  },
+  {
+    "revision": "9542809542ad5daedf78",
+    "url": "/react-dianping/static/css/3.ec8f39b5.chunk.css"
+  },
+  {
+    "revision": "ef2b60c1985e8f1c42d5",
+    "url": "/react-dianping/static/css/5.24f7a022.chunk.css"
+  },
+  {
+    "revision": "9fcdb16804f380e88e45",
+    "url": "/react-dianping/static/css/6.ca24c989.chunk.css"
+  },
+  {
+    "revision": "457921e56ec53a98ce1f",
+    "url": "/react-dianping/static/css/7.62872d25.chunk.css"
+  },
+  {
+    "revision": "982a036220953db07785",
+    "url": "/react-dianping/static/css/8.6d642f0e.chunk.css"
+  },
+  {
+    "revision": "10cf11e70945411db77a",
+    "url": "/react-dianping/static/css/9.5009854e.chunk.css"
+  },
+  {
+    "revision": "7b29608d9567a5720d57",
+    "url": "/react-dianping/static/css/main.055e1af8.chunk.css"
+  },
+  {
+    "revision": "6ad4bd9ab94bafa31f70",
+    "url": "/react-dianping/static/js/10.75a15201.chunk.js"
+  },
+  {
+    "revision": "3d792787c618ec616665",
+    "url": "/react-dianping/static/js/2.7579fb8c.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/react-dianping/static/js/2.7579fb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9542809542ad5daedf78",
+    "url": "/react-dianping/static/js/3.76bf5250.chunk.js"
+  },
+  {
+    "revision": "ac36484d0213333a9ee8",
+    "url": "/react-dianping/static/js/4.15953683.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/react-dianping/static/js/4.15953683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2b60c1985e8f1c42d5",
+    "url": "/react-dianping/static/js/5.577a16f9.chunk.js"
+  },
+  {
+    "revision": "9fcdb16804f380e88e45",
+    "url": "/react-dianping/static/js/6.1c929fe1.chunk.js"
+  },
+  {
+    "revision": "457921e56ec53a98ce1f",
+    "url": "/react-dianping/static/js/7.17cae94c.chunk.js"
+  },
+  {
+    "revision": "982a036220953db07785",
+    "url": "/react-dianping/static/js/8.eace762c.chunk.js"
+  },
+  {
+    "revision": "10cf11e70945411db77a",
+    "url": "/react-dianping/static/js/9.f5f2aae8.chunk.js"
+  },
+  {
+    "revision": "7b29608d9567a5720d57",
+    "url": "/react-dianping/static/js/main.fd15a88c.chunk.js"
+  },
+  {
+    "revision": "0b89931262edb666c9d8",
+    "url": "/react-dianping/static/js/runtime-main.881cca10.js"
+  }
+]);
